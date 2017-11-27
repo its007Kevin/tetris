e9a407a7d0279e9044c02468252e7140df70dec3
its007Kevin <kevinzeng98@gmail.com>
2017-11-27 17:11:27 -0500

    Merge with Bob's code

--- main.cc
+++ main.cc
@@ -1,11 +1,7 @@
 #include <iostream>
 #include <string>
-<<<<<<< HEAD
 #include "grid.h"
 #include "textdisplay.h"
-=======
-#include "graphicsdisplay.h"
->>>>>>> cc9e01c533d3f0e1e5fd0b84be918fb56b39a2e2
 using namespace std;
 
 int main(int argc, char *argv[]) {
@@ -29,80 +25,74 @@ int main(int argc, char *argv[]) {
   string res = "restart";
   string hint = "hint";
 
-
   Grid g;
 
-  g.initialize();
-  try {
-<<<<<<< HEAD
-    int r = 18;
-    int c = 11;
-    TextDisplay *td = new TextDisplay{r, c};
-    g.setTextDisplay(td);
-    g.init(r, c);
-    cout << g;
-    while (true) {
+  int r = 18;
+  int c = 11;
+  TextDisplay *td = new TextDisplay{r, c};
+  g.setTextDisplay(td);
+  g.init(r, c);
+  cout << g;
 
-=======
-  while (cin >> cmd) {
-    if (cmd == left) {
-      g.moveLeft();
-    }
-    else if (cmd == right) {
-      g.moveRight();
-    }
-    else if (cmd == down) {
-      g.moveDown();
-    }
-    else if (cmd == drop) {
-      g.moveDrop();
-    }
-    else if (cmd == cw) {
-      g.rotateCW();
-    }
-    else if (cmd == ccw) {
-      g.rotateCCW();
-    }
-    else if (cmd == lvlup) {
-      g.levelUp();
-    }
-    else if (cmd == lvldown) {
-      g.levelDown();
->>>>>>> cc9e01c533d3f0e1e5fd0b84be918fb56b39a2e2
-    }
-    else if (cmd == norand) {
-      string file;
-      cin >> file;
-      g.noRandom(file);
-    }
-    else if (cmd == rand) {
-      g.random();
-    }
-    else if (cmd == seq) {
-      string file;
-      cin >> file;
-      g.sequence(file);
-    }
-    else if (cmd == I) {
-      g.replaceWithI();
-    }
-    else if (cmd == J) {
-      g.replaceWithJ();
-    }
-    else if (cmd == L) {
-      g.replaceWithL();
-    }
-    else if (cmd == res) {
-      g.restart();
-    }
-    else if (cmd == hint) {
-      g.hint();
-    }
-    else {
-      cout << "Invalid input" << endl;
-    }
-  }
-  }
-  catch (ios::failure &) {}  // Any I/O failure quits
-  cout << "Game Over!" << endl;
+  // try {
+  // while (cin >> cmd) {
+  //   if (cmd == left) {
+  //     g.moveLeft();
+  //   }
+  //   else if (cmd == right) {
+  //     g.moveRight();
+  //   }
+  //   else if (cmd == down) {
+  //     g.moveDown();
+  //   }
+  //   else if (cmd == drop) {
+  //     g.moveDrop();
+  //   }
+  //   else if (cmd == cw) {
+  //     g.rotateCW();
+  //   }
+  //   else if (cmd == ccw) {
+  //     g.rotateCCW();
+  //   }
+  //   else if (cmd == lvlup) {
+  //     g.levelUp();
+  //   }
+  //   else if (cmd == lvldown) {
+  //     g.levelDown();
+  //   }
+  //   else if (cmd == norand) {
+  //     string file;
+  //     cin >> file;
+  //     g.noRandom(file);
+  //   }
+  //   else if (cmd == rand) {
+  //     g.random();
+  //   }
+  //   else if (cmd == seq) {
+  //     string file;
+  //     cin >> file;
+  //     g.sequence(file);
+  //   }
+  //   else if (cmd == I) {
+  //     g.replaceWithI();
+  //   }
+  //   else if (cmd == J) {
+  //     g.replaceWithJ();
+  //   }
+  //   else if (cmd == L) {
+  //     g.replaceWithL();
+  //   }
+  //   else if (cmd == res) {
+  //     g.restart();
+  //   }
+  //   else if (cmd == hint) {
+  //     g.hint();
+  //   }
+  //   else {
+  //     cout << "Invalid input" << endl;
+  //   }
+  // }
+  // }
+  // catch (ios::failure &) {}  // Any I/O failure quits
+  // cout << "Game Over!" << endl;
 }
