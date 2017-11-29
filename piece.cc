8db338944da7b1e20bcc776fa2815e145a1a1cd5
Bob <bob.zhang.333@gmail.com>
2017-11-29 17:15:13 -0500

    Rotation functionality!

--- piece.cc
+++ piece.cc
@@ -8,55 +8,30 @@ using namespace std;
 Piece::Piece(char type): type{type} {
   if (type == 'I') {
     coords = {{0, 0}, {1, 0}, {2, 0}, {3, 0}};
-    //botLefty = 3;
-    //height = 4;
-    //width = 1;
   }
   else if (type == 'J') {
-//    coords = {{2, 3}, {0, 4}, {1, 4}, {2, 4}};
-coords = {{2, 0}, {0, 1}, {1, 1}, {2, 1}};
-    //botLefty = 1;
-    //height = 3;
-    //width = 2;
+    coords = {{2, 0}, {0, 1}, {1, 1}, {2, 1}};
   }
   else if (type == 'L') {
     coords = {{0, 0}, {1, 0}, {2, 0}, {2,1}};
-    //botLefty = 1;
-    //height = 3;
-    //width = 2;
   }
   else if (type == 'O') {
     coords = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
-    //botLefty = 1;
-    //height = 2;
-    //width = 2;
   }
   else if (type == 'S') {
     coords = {{1, 0}, {0, 2}, {0, 1}, {1, 1}};
-    //botLefty = 1;
-    //height = 2;
-    //width = 3;
   }
   else if (type == 'Z') {
     coords = {{0, 0}, {0, 1}, {1,1}, {1, 2}};
-    //botLefty = 1;
-    //height = 2;
-    //width = 3;
   }
   else if (type == 'T') {
     coords = {{0, 0}, {0, 1}, {0, 2}, {1, 1}};
-    //botLefty = 1;
-    //height = 2;
-    //width = 3;
   }
   else if (type == '*') {
     coords = {{0, 0}};
-    //height = 1;
-    //width = 1;
   }
   potentialCoords = coords;
 }
-//rotate: find lowest x, y coord, add 4 to it
 
 void Piece::makeIsHeavy() {
   isHeavy = true;
@@ -73,23 +48,17 @@ void Piece::rotateCW() {
     if (potentialCoords[i][1] > xMax) xMax = potentialCoords[i][1];
     if (potentialCoords[i][1] < xMin) xMin = potentialCoords[i][1];
   }
-  //int botLeftx;
-  //int botLefty;
-  int width = xMax - xMin;
-  int height = yMax - yMin;
+  int width = xMax - xMin + 1;
+  int height = yMax - yMin + 1;
   for (int i = 0; i < potentialCoords.size(); i++) {
     potentialCoords[i][0] = potentialCoords[i][0] - yMax + height - 1;
     potentialCoords[i][1] -= xMin;
     // transpose
-    //coords[i].reverse(0, coords[i].size());
     int temp = potentialCoords[i][0];
     potentialCoords[i][0] = potentialCoords[i][1];
     potentialCoords[i][1] = temp;
-    //
     potentialCoords[i][1] = height - potentialCoords[i][1] - 1;
-
-    potentialCoords[i][0] = potentialCoords[i][0] + xMax - width + 1;
-    if (width > height) potentialCoords[i][0] -= width - height;
+    potentialCoords[i][0] = potentialCoords[i][0] + yMax - height + 1 + height - width;
     potentialCoords[i][1] += xMin;
   }
 }
@@ -105,58 +74,19 @@ void Piece::rotateCCW() {
     if (potentialCoords[i][1] > xMax) xMax = potentialCoords[i][1];
     if (potentialCoords[i][1] < xMin) xMin = potentialCoords[i][1];
   }
-  //int botLeftx;
-  //int botLefty;
-  int width = xMax - xMin;
-  int height = yMax - yMin;
+  int width = xMax - xMin + 1;
+  int height = yMax - yMin + 1;
   for (int i = 0; i < potentialCoords.size(); i++) {
     potentialCoords[i][0] = potentialCoords[i][0] - yMax + height - 1;
     potentialCoords[i][1] -= xMin;
-    // transpose
-    //coords[i].reverse(0, coords[i].size());
     int temp = potentialCoords[i][0];
     potentialCoords[i][0] = potentialCoords[i][1];
     potentialCoords[i][1] = temp;
-    //
     potentialCoords[i][0] = width - potentialCoords[i][0] - 1;
-
-    potentialCoords[i][0] = potentialCoords[i][0] + xMax - width + 1;
-    if (width > height) potentialCoords[i][0] -= width - height;
-    potentialCoords[i][1] += xMin;
+    potentialCoords[i][0] = potentialCoords[i][0] + yMax - height + 1 + height - width;
+    potentialCoords[i][1] = potentialCoords[i][1] + xMax - width + 1 - height + width;
   }
 }
-
-// void Piece::OLDrotate(bool isCw) {
-//   std::vector<std::vector<int>> newcoords;
-//   if (isCw) {
-//     int sizeInner = coords[0].size();
-//     for (int i = 0; i < sizeInner; i++) {
-//       std::vector<int> newRow;
-//       int sizeOuter = coords.size();
-//       for (int j = sizeOuter - 1; j >= 0; j--) {
-//         newRow.emplace_back(coords[j][i]);
-//       }
-//       newcoords.emplace_back(newRow);
-
-//     }
-//   }
-//   else {
-//     int sizeInner = coords[0].size();
-//     for (int i = sizeInner - 1; i >= 0; i--) {
-//       std::vector<int> newRow;
-//       int sizeOuter = coords.size();
-//       for (int j = 0; j < sizeOuter; j++) {
-//         newRow.emplace_back(coords[j][i]);
-//       }
-//       newcoords.emplace_back(newRow);
-//     }
-//     botLeftx -= height;
-//   }
-//   coords = newcoords;
-//   int temp = height;
-//   height = width;
-//   width = temp;
-// }
 
 vector<vector<int>> Piece::getCoords() {
   return coords;
