28da91126a418b5c4b88e72f8c2d949b3fc1def5
its007Kevin <kevinzeng98@gmail.com>
2017-11-30 23:22:21 -0500

    rows to delete

--- grid.cc
+++ grid.cc
@@ -184,6 +184,8 @@ void Grid::spawnNextPiece() {
 
 void Grid::removeFilledRows() {
     bool isFilled;
+    vector<int> rowsToDelete;
+    // Checks for rows to delete
     for (int i = 0; i < theGrid.size(); i++) {
         isFilled = true;
         for (int j = 0; j < theGrid[i].size(); j++) {
@@ -192,22 +194,17 @@ void Grid::removeFilledRows() {
             }
         }
         if (isFilled == true) {
-            theGrid.erase(theGrid.begin() + i);
-            vector<Cell> emptyRow;
-            for (int j = 0; j < cols; j++) {
-                Cell cell{-1, j, '-'};
-                cell.attach(td);
-                emptyRow.emplace_back(cell);
-            }
-            theGrid.emplace(theGrid.begin(), emptyRow);
+            rowsToDelete.emplace_back(i);
         }
     }
-    for (int i = -1; i < theGrid.size(); i++) {
-        for (int j = 0; j < theGrid[i].size(); j++) {
-            theGrid[i][j].notifyObservers();
-            theGrid[i][j].shiftRows();
+    for (int i = 0; i < rowsToDelete.size(); i++) {
+        for (int j = 0; j < cols; j++) {
+            theGrid[rowsToDelete.at(i)][j].setData('-');
         }
     }
+    // for (int i = 0; i < rowsToDelete.size(); i++) {
+    //     theGrid.erase(theGrid.begin() + rowsToDelete.at(i) - i);
+    // }
 }
 
 void Grid::levelUp() {
